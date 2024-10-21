c5933816283c3109cf90b1f508883bd5a90cc3e1
Jonatas <jonatas.melo@3lminformatica.com.br>
2024-10-21 08:35:27 -0300

    style: clean import

--- src/main/java/com/management/finance/error/ExceptionHandlerController.java
+++ src/main/java/com/management/finance/error/ExceptionHandlerController.java
@@ -2,7 +2,6 @@ package com.management.finance.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
--- src/main/java/com/management/finance/user/UserRepository.java
+++ src/main/java/com/management/finance/user/UserRepository.java
@@ -3,7 +3,6 @@ package com.management.finance.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
