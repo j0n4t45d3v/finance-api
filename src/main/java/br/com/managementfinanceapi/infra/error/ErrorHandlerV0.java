0eabd8110c92af8d95b46c189686e3b919da2639
Jonatas <delimajonatas875@gmail.com>
2025-03-06 10:37:26 -0300

    feat: error handler version 0

---
+++ src/main/java/br/com/managementfinanceapi/infra/error/ErrorHandlerV0.java
@@ -0,0 +1,44 @@
+package br.com.managementfinanceapi.infra.error;
+
+import br.com.managementfinanceapi.infra.error.exceptions.BaseException;
+import br.com.managementfinanceapi.infra.http.dto.ErrorV0;
+import br.com.managementfinanceapi.infra.http.dto.ResponseV0;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class ErrorHandlerV0 {
+
+  private static final Logger log = LoggerFactory.getLogger(ErrorHandlerV0.class);
+
+  @ExceptionHandler(BaseException.class)
+  public ResponseEntity<ResponseV0<ErrorV0>> handleBaseException(BaseException e) {
+    ResponseV0<ErrorV0> responseError = ResponseV0.error(e.getCode(), e);
+    log.error("Custom Error: {}", e.getMessage());
+    return ResponseEntity.status(e.getCode()).body(responseError);
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<Void> handleNoResource(NoResourceFoundException e) {
+    return ResponseEntity.notFound().build();
+  }
+
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<ResponseV0<ErrorV0>> handleMethodNotAllowed(HttpRequestMethodNotSupportedException e) {
+    ResponseV0<ErrorV0> responseError = ResponseV0.error(405, e);
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(responseError);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ResponseV0<ErrorV0>> handleGenericException(Exception e) {
+    ResponseV0<ErrorV0> responseError = ResponseV0.error(500, e);
+    return ResponseEntity.internalServerError().body(responseError);
+  }
+}
