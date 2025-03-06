bfbd62156932e5a66f6c47caa04ed66661c21ce3
Jonatas <delimajonatas875@gmail.com>
2025-03-06 10:33:41 -0300

    initial commit

---
+++ src/test/java/br/com/managementfinanceapi/ManagementFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.managementfinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementFinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
