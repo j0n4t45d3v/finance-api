f4af4b77c3a3719e4e7383a489a9aa6406581c18
Jonatas <delimajonatas875@gmail.com>
2025-03-10 19:20:21 -0300

    feat: balance entity

---
+++ src/main/java/br/com/managementfinanceapi/transaction/domain/Balance.java
@@ -0,0 +1,62 @@
+package br.com.managementfinanceapi.transaction.domain;
+
+import br.com.managementfinanceapi.user.domain.User;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "balances")
+public class Balance {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private BigDecimal amount;
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+  private Short month;
+  private Short year;
+
+  public Balance() {
+  }
+
+  public Balance(Long id, BigDecimal amount, User user, Short month, Short year) {
+    this.id = id;
+    this.amount = amount;
+    this.user = user;
+    this.month = month;
+    this.year = year;
+  }
+
+  public Balance(BigDecimal amount, User user, Short month, Short year) {
+    this.amount = amount;
+    this.user = user;
+    this.month = month;
+    this.year = year;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public Short getMonth() {
+    return month;
+  }
+
+  public Short getYear() {
+    return year;
+  }
+
+  public void addAmount(BigDecimal amount) {
+    this.amount = this.amount.add(amount);
+  }
+}
