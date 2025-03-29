cb50cfc42e3800a46bca2c756cbf77f992af39d7
Jonatas <delimajonatas875@gmail.com>
2025-03-29 11:07:52 -0300

    chore: remove logs

--- src/main/java/br/com/managementfinanceapi/transaction/controller/TransactionControllerV1.java
+++ src/main/java/br/com/managementfinanceapi/transaction/controller/TransactionControllerV1.java
@@ -1,30 +1,20 @@
 package br.com.managementfinanceapi.transaction.controller;
 
-import br.com.managementfinanceapi.transaction.domain.dtos.TransactionDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.managementfinanceapi.infra.http.dto.ResponseV0;
 import br.com.managementfinanceapi.transaction.domain.dtos.AddTransaction;
 import br.com.managementfinanceapi.transaction.domain.dtos.SearchTransaction;
+import br.com.managementfinanceapi.transaction.domain.dtos.TransactionDto;
 import br.com.managementfinanceapi.transaction.gateways.AddTransactionGateway;
 import br.com.managementfinanceapi.transaction.gateways.FindAllTransactionGateway;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/transactions")
 public class TransactionControllerV1 {
-
-  private static final Logger log = LoggerFactory.getLogger(TransactionControllerV1.class);
 
   private final AddTransactionGateway addTransactionGateway;
   private final FindAllTransactionGateway findAllTransactionGateway;
@@ -48,9 +38,6 @@ public class TransactionControllerV1 {
   ) {
     var result = this.findAllTransactionGateway.findAll(filters, page);
     var addMovementResponse = ResponseV0.ok(result);
-    log.info("userId: {}", filters.userId());
-    log.info("startDate: {}", filters.startDate());
-    log.info("endDate: {}", filters.endDate());
     return ResponseEntity.ok(addMovementResponse);
   }
 }
