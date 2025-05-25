d240dccd278a7e3f5de6fb5a18d9176248591622
Jonatas <delimajonatas875@gmail.com>
2025-05-25 11:21:03 -0300

    style: remove files unuseds

--- src/main/java/br/com/managementfinanceapi/application/core/domain/transaction/dtos/UpdateBalance.java
+++
@@ -1,6 +0,0 @@
-package br.com.managementfinanceapi.application.core.domain.transaction.dtos;
-
-import java.math.BigDecimal;
-
-public record UpdateBalance(BigDecimal amount, short month, short year, Long userId) {
-}
