ae5dd1a28bada4fabf29d894b3edea2208bd77d9
Jonatas <delimajonatas875@gmail.com>
2025-05-28 20:15:37 -0300

    style: separete beans

--- src/main/java/br/com/managementfinanceapi/infra/bean/transaction/BalanceBean.java
+++ src/main/java/br/com/managementfinanceapi/infra/bean/transaction/BalanceBean.java
@@ -4,16 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import br.com.managementfinanceapi.application.core.usecase.transaction.AddCurrentAccountBalanceUseCase;
-import br.com.managementfinanceapi.application.core.usecase.transaction.AddTransactionUseCase;
-import br.com.managementfinanceapi.application.core.usecase.transaction.SearchTransactionUseCase;
 import br.com.managementfinanceapi.application.core.usecase.transaction.UpdateBalanceOfMonthUseCase;
 import br.com.managementfinanceapi.application.port.in.transaction.AddCurrentAccountBalancePort;
-import br.com.managementfinanceapi.application.port.in.transaction.AddTransactionPort;
-import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
 import br.com.managementfinanceapi.application.port.in.transaction.UpdateBalanceOfMonthPort;
-import br.com.managementfinanceapi.application.port.in.user.SearchUserPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SaveBalanceRepositoryPort;
-import br.com.managementfinanceapi.application.port.out.transaction.SaveTransactionRepositoryPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SearchBalanceRepositoryPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SearchTransactionRespositoryPort;
 
@@ -27,22 +21,6 @@ public class BalanceBean {
       SearchTransactionRespositoryPort searchTransactionPort
   ) {
     return new AddCurrentAccountBalanceUseCase(searchBalanceRepository, saveBalanceRepository, searchTransactionPort);
-  }
-
-  @Bean
-  public AddTransactionPort addTransaction(
-      SaveTransactionRepositoryPort saveTransactionRepositoryPort,
-      SearchUserPort searchUserPort,
-      UpdateBalanceOfMonthPort updateBalanceOfMonth
-  ) {
-    return new AddTransactionUseCase(saveTransactionRepositoryPort, searchUserPort, updateBalanceOfMonth);
-  }
-
-  @Bean
-  public SearchTransactionPort searchTransaction(
-      SearchTransactionRespositoryPort searchRepositoryPort
-  ) {
-    return new SearchTransactionUseCase(searchRepositoryPort);
   }
 
   @Bean
--- src/main/java/br/com/managementfinanceapi/infra/bean/transaction/TransactionBean.java
+++ src/main/java/br/com/managementfinanceapi/infra/bean/transaction/TransactionBean.java
@@ -1,38 +1,22 @@
 package br.com.managementfinanceapi.infra.bean.transaction;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-import br.com.managementfinanceapi.application.core.usecase.transaction.AddCurrentAccountBalanceUseCase;
 import br.com.managementfinanceapi.application.core.usecase.transaction.AddTransactionUseCase;
 import br.com.managementfinanceapi.application.core.usecase.transaction.SearchTransactionUseCase;
-import br.com.managementfinanceapi.application.core.usecase.transaction.UpdateBalanceOfMonthUseCase;
-import br.com.managementfinanceapi.application.port.in.transaction.AddCurrentAccountBalancePort;
 import br.com.managementfinanceapi.application.port.in.transaction.AddTransactionPort;
 import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
 import br.com.managementfinanceapi.application.port.in.transaction.UpdateBalanceOfMonthPort;
 import br.com.managementfinanceapi.application.port.in.user.SearchUserPort;
-import br.com.managementfinanceapi.application.port.out.transaction.SaveBalanceRepositoryPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SaveTransactionRepositoryPort;
-import br.com.managementfinanceapi.application.port.out.transaction.SearchBalanceRepositoryPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SearchTransactionRespositoryPort;
 
+@Component
 public class TransactionBean {
 
   @Bean
-  public AddCurrentAccountBalancePort addCurrentAccountBalance(
-      SearchBalanceRepositoryPort searchBalanceRepository,
-      SaveBalanceRepositoryPort saveBalanceRepository,
-      SearchTransactionRespositoryPort searchTransactionPort
-  ) {
-    return new AddCurrentAccountBalanceUseCase(
-      searchBalanceRepository,
-      saveBalanceRepository,
-      searchTransactionPort
-    );
-  }
-
-  @Bean
-  public AddTransactionPort adTransaction(
+  public AddTransactionPort addTransaction(
       SaveTransactionRepositoryPort saveTransactionRepositoryPort,
       SearchUserPort searchUserPort,
       UpdateBalanceOfMonthPort updateBalanceOfMonth
@@ -51,14 +35,4 @@ public class TransactionBean {
     return new SearchTransactionUseCase(searchTransactionRespositoryPort);
   }
 
-  @Bean
-  public UpdateBalanceOfMonthPort updateBalanceOfMonth(
-    SearchBalanceRepositoryPort searchBalanceRepositoryPort,
-    SaveBalanceRepositoryPort saveBalanceRepositoryPort
-  ) {
-    return new UpdateBalanceOfMonthUseCase(
-      searchBalanceRepositoryPort,
-      saveBalanceRepositoryPort
-    );
-  }
 }
