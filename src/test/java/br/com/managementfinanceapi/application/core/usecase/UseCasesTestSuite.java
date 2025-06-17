74fe34ab8fb98c1798e5425d7724d89fce80510b
Jonatas <jonatas.melo@3lminformatica.com.br>
2025-06-17 15:57:25 -0300

    test: usecase suite

---
+++ src/test/java/br/com/managementfinanceapi/application/core/usecase/UseCasesTestSuite.java
@@ -0,0 +1,10 @@
+package br.com.managementfinanceapi.application.core.usecase;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Use Case Test Suite")
+@SelectPackages("br.com.managementfinanceapi.application.core.usecase")
+public class UseCasesTestSuite {}
