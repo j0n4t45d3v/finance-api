a263eb1d4ce2dd2cccd70ce0e3366e2485633f05
Jonatas <delimajonatas875@gmail.com>
2025-06-24 10:44:18 -0300

    chore: add find income and expence totals by category

--- src/main/java/br/com/managementfinanceapi/adapter/out/repository/category/SearchCategoryRepositoryImpl.java
+++ src/main/java/br/com/managementfinanceapi/adapter/out/repository/category/SearchCategoryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package br.com.managementfinanceapi.adapter.out.repository.category;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Component;
 import br.com.managementfinanceapi.adapter.out.entity.category.CategoryEntity;
 import br.com.managementfinanceapi.adapter.out.mapper.Mapper;
 import br.com.managementfinanceapi.application.core.domain.category.CategoryDomain;
+import br.com.managementfinanceapi.application.core.domain.category.dvo.CategoryTransactionSummary;
 import br.com.managementfinanceapi.application.port.out.category.SearchCategoryRepositoryPort;
+import jakarta.annotation.Nonnull;
 
 @Component
 public class SearchCategoryRepositoryImpl implements SearchCategoryRepositoryPort{
@@ -36,6 +39,15 @@ public class SearchCategoryRepositoryImpl implements SearchCategoryRepositoryPor
   public Optional<CategoryDomain> byUserIdAndName(Long userId, String name) {
     return this.repository.findByUserIdAndName(userId, name)
                           .map(this.mapper::toDomain);
+  }
+
+  @Override
+  public List<CategoryTransactionSummary> getSummaryIncomeAndExpencesTotals(
+    @Nonnull Long userId, 
+    @Nonnull LocalDateTime startDate,
+    @Nonnull LocalDateTime endDate
+  ) {
+    return this.repository.findIncomesAndExpensesTotals(startDate, endDate, userId);
   }
 
   
--- src/main/java/br/com/managementfinanceapi/application/port/in/category/SearchCategoryPort.java
+++ src/main/java/br/com/managementfinanceapi/application/port/in/category/SearchCategoryPort.java
@@ -1,10 +1,13 @@
 package br.com.managementfinanceapi.application.port.in.category;
 
-import br.com.managementfinanceapi.application.core.domain.category.CategoryDomain;
-
 import java.util.List;
+
+import br.com.managementfinanceapi.application.core.domain.category.CategoryDomain;
+import br.com.managementfinanceapi.application.core.domain.category.dvo.CategoryTransactionSummary;
+import br.com.managementfinanceapi.application.core.domain.common.dvo.DateRange;
 
 public interface SearchCategoryPort {
   List<CategoryDomain> all();
   CategoryDomain byName(Long userId, String name);
+  List<CategoryTransactionSummary> getSummaryIncomeAndExpencesTotals(Long userId, DateRange dateRange);
 }
