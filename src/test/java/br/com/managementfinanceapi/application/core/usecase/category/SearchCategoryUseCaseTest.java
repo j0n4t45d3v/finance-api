3df8f9523b8b1787a17ff48b5cfe10d19f9fd6cb
Jonatas <delimajonatas875@gmail.com>
2025-06-24 11:23:05 -0300

    test: income and expence totals summary by category

--- src/test/java/br/com/managementfinanceapi/application/core/usecase/category/SearchCategoryUseCaseTest.java
+++ src/test/java/br/com/managementfinanceapi/application/core/usecase/category/SearchCategoryUseCaseTest.java
@@ -1,8 +1,21 @@
 package br.com.managementfinanceapi.application.core.usecase.category;
 
-import br.com.managementfinanceapi.application.core.domain.category.CategoryDomain;
-import br.com.managementfinanceapi.application.core.domain.category.exception.CategoryDoesNotExistsException;
-import br.com.managementfinanceapi.application.port.out.category.SearchCategoryRepositoryPort;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,11 +23,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import br.com.managementfinanceapi.application.core.domain.category.CategoryDomain;
+import br.com.managementfinanceapi.application.core.domain.category.dvo.CategoryTransactionSummary;
+import br.com.managementfinanceapi.application.core.domain.category.exception.CategoryDoesNotExistsException;
+import br.com.managementfinanceapi.application.core.domain.common.dvo.DateRange;
+import br.com.managementfinanceapi.application.port.out.category.SearchCategoryRepositoryPort;
 
 @ExtendWith(MockitoExtension.class)
 class SearchCategoryUseCaseTest {
@@ -56,5 +69,30 @@ class SearchCategoryUseCaseTest {
     assertEquals("Categoria não existe!", thrown.getMessage());
     verify(this.searchCategoryRepositoryPort, times(1))
         .byUserIdAndName(anyLong(), anyString());
+  }
+
+  @Test
+  @DisplayName("should return all transaction grouped by category")
+  void shouldReturnAllTransactionGroupedByCategory() {
+    CategoryTransactionSummary categoryTransactionSummaryMock = mock(CategoryTransactionSummary.class);
+    when(this.searchCategoryRepositoryPort.getSummaryIncomeAndExpencesTotals(
+        anyLong(),
+        any(LocalDateTime.class),
+        any(LocalDateTime.class)
+      )).thenReturn(List.of(categoryTransactionSummaryMock));
+
+    DateRange period = new DateRange(LocalDate.now(), LocalDate.now());
+    List<CategoryTransactionSummary> result = 
+      this.searchCategoryUseCase.getSummaryIncomeAndExpencesTotals(1L, period);
+
+    assertEquals(1, result.size());
+    assertNotNull(result.get(0));
+
+    verify(this.searchCategoryRepositoryPort, times(1))
+      .getSummaryIncomeAndExpencesTotals(
+        anyLong(),
+        any(LocalDateTime.class),
+        any(LocalDateTime.class)
+      );
   }
 }
