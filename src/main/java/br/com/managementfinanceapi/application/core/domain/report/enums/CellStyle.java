d730305630948a616ed7737098e1dc62ba913884
Jonatas <delimajonatas875@gmail.com>
2025-06-24 10:37:02 -0300

    chore: add new styles

--- src/main/java/br/com/managementfinanceapi/application/core/domain/report/enums/CellStyle.java
+++ src/main/java/br/com/managementfinanceapi/application/core/domain/report/enums/CellStyle.java
@@ -3,6 +3,10 @@ package br.com.managementfinanceapi.application.core.domain.report.enums;
 public enum CellStyle {
   HEADER,
   MONEY_FORMAT,
+  PERCENTEGE_FORMAT,
+  WARN,
+  PAINT_INCOME,
+  PAINT_EXPENCE,
   ALTERNETE_ROW,
   DATE_FORMAT,
   TIMESTAMP_FORMAT,
