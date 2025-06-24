307ff2b9b79e1cdcf7b8c16fba27ff2729c18448
Jonatas <delimajonatas875@gmail.com>
2025-06-24 11:10:48 -0300

    style: cleanup imports

--- src/test/java/br/com/managementfinanceapi/application/core/usecase/category/SearchCategoryUseCaseTest.java
+++ src/test/java/br/com/managementfinanceapi/application/core/usecase/category/SearchCategoryUseCaseTest.java
@@ -8,10 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
