5d6d58c91558a70960827b544c212f5a275c7754
Jonatas <delimajonatas875@gmail.com>
2025-06-24 10:47:06 -0300

    feat: page resume total transaction by category

--- src/main/java/br/com/managementfinanceapi/application/core/usecase/report/excel/FinanceReportUseCase.java
+++ src/main/java/br/com/managementfinanceapi/application/core/usecase/report/excel/FinanceReportUseCase.java
@@ -1,28 +1,55 @@
 package br.com.managementfinanceapi.application.core.usecase.report.excel;
 
+import java.util.List;
+
+import br.com.managementfinanceapi.application.core.domain.category.dvo.CategoryTransactionSummary;
 import br.com.managementfinanceapi.application.core.domain.common.dvo.DateRange;
+import br.com.managementfinanceapi.application.core.domain.report.ReportTable;
+import br.com.managementfinanceapi.application.core.domain.transaction.TransactionDomain;
+import br.com.managementfinanceapi.application.port.in.category.SearchCategoryPort;
 import br.com.managementfinanceapi.application.port.in.report.CreateReportPagePort;
 import br.com.managementfinanceapi.application.port.in.report.FinanceReportPort;
+import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
 import br.com.managementfinanceapi.application.port.out.report.ReportGeneratorPort;
 
 public class FinanceReportUseCase implements FinanceReportPort {
 
-  public final ReportGeneratorPort reportGeneratorPort;
-  public final CreateReportPagePort createReportPagePort;
+  private final ReportGeneratorPort reportGeneratorPort;
+  private final SearchTransactionPort searchTransactionPort;
+  private final SearchCategoryPort searchCategoryPort;
+  private final CreateReportPagePort<List<TransactionDomain>> pageTrasactions;
+  private final CreateReportPagePort<List<CategoryTransactionSummary>> pageResumeByCategory;
 
   public FinanceReportUseCase(
-      ReportGeneratorPort reportGeneratorPort,
-      CreateReportPagePort createReportPagePort
+    ReportGeneratorPort reportGeneratorPort,
+    SearchTransactionPort searchTransactionPort,
+    SearchCategoryPort searchCategoryPort,
+    CreateReportPagePort<List<TransactionDomain>> pageTrasactions,
+    CreateReportPagePort<List<CategoryTransactionSummary>> pageResumeByCategory
   ) {
     this.reportGeneratorPort = reportGeneratorPort;
-    this.createReportPagePort = createReportPagePort;
+    this.searchTransactionPort = searchTransactionPort;
+    this.searchCategoryPort = searchCategoryPort;
+    this.pageTrasactions = pageTrasactions;
+    this.pageResumeByCategory = pageResumeByCategory;
   }
 
   @Override
   public byte[] generate(Long userId, DateRange dateRange) {
     return this.reportGeneratorPort
-        .addPage(this.createReportPagePort.generate(userId, dateRange))
+        .addPage(this.createPageTrasactions(userId, dateRange))
+        .addPage(this.createPageCategorySummaryTotals(userId, dateRange))
         .generate();
   }
 
+  private ReportTable createPageTrasactions(Long userId, DateRange dateRange) {
+    List<TransactionDomain> transactions = this.searchTransactionPort.allByUser(userId, dateRange);
+    return this.pageTrasactions.generate(transactions);
+  }
+
+  private ReportTable createPageCategorySummaryTotals(Long userId, DateRange dateRange) {
+    List<CategoryTransactionSummary> categoryTransactionSummaries = 
+    this.searchCategoryPort.getSummaryIncomeAndExpencesTotals(userId, dateRange);
+    return this.pageResumeByCategory.generate(categoryTransactionSummaries);
+  }
 }
--- src/main/java/br/com/managementfinanceapi/infra/bean/report/ReportBean.java
+++ src/main/java/br/com/managementfinanceapi/infra/bean/report/ReportBean.java
@@ -1,11 +1,16 @@
 package br.com.managementfinanceapi.infra.bean.report;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import br.com.managementfinanceapi.application.core.domain.category.dvo.CategoryTransactionSummary;
 import br.com.managementfinanceapi.application.core.domain.transaction.TransactionDomain;
 import br.com.managementfinanceapi.application.core.usecase.report.excel.FinanceReportUseCase;
+import br.com.managementfinanceapi.application.core.usecase.report.excel.PageResumeByCategory;
 import br.com.managementfinanceapi.application.core.usecase.report.excel.PageTransaction;
+import br.com.managementfinanceapi.application.port.in.category.SearchCategoryPort;
 import br.com.managementfinanceapi.application.port.in.report.CreateReportPagePort;
 import br.com.managementfinanceapi.application.port.in.report.FinanceReportPort;
 import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
@@ -15,15 +20,28 @@ import br.com.managementfinanceapi.application.port.out.report.ReportGeneratorPo
 public class ReportBean {
 
   @Bean
-  public CreateReportPagePort createPage(SearchTransactionPort searchTransactionPort) {
-  return new PageTransaction(searchTransactionPort);
   public CreateReportPagePort<List<TransactionDomain>> createPageTransactions() {
     return new PageTransaction();
   }
   @Bean
+  public CreateReportPagePort<List<CategoryTransactionSummary>> createPageResumeTransactionByCategory() {
+    return new PageResumeByCategory();
+  }
 
   @Bean
-  public FinanceReportPort financeReport(ReportGeneratorPort reportGeneratorPort, CreateReportPagePort createReportPagePort) {
-  return new FinanceReportUseCase(reportGeneratorPort, createReportPagePort);
+  public FinanceReportPort financeReport(
+    ReportGeneratorPort reportGeneratorPort,
+    SearchTransactionPort searchTransactionPort,
+    SearchCategoryPort searchCategoryPort,
+    CreateReportPagePort<List<TransactionDomain>> pageTrasactions,
+    CreateReportPagePort<List<CategoryTransactionSummary>> pageResumeByCategory
+  ) {
+    return new FinanceReportUseCase(
+      reportGeneratorPort,
+      searchTransactionPort,
+      searchCategoryPort,
+      pageTrasactions,
+      pageResumeByCategory
+    );
   }
 }
