88218ba1290680b2288e08691d489acfa4ca03ba
Jonatas <delimajonatas875@gmail.com>
2025-06-28 12:23:59 -0300

    style: cleanup imports

--- src/main/java/br/com/managementfinanceapi/application/core/usecase/transaction/SearchTransactionUseCase.java
+++ src/main/java/br/com/managementfinanceapi/application/core/usecase/transaction/SearchTransactionUseCase.java
@@ -1,19 +1,15 @@
 package br.com.managementfinanceapi.application.core.usecase.transaction;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import br.com.managementfinanceapi.application.core.domain.common.dvo.DateRange;
 import br.com.managementfinanceapi.application.core.domain.common.dvo.Page;
-import br.com.managementfinanceapi.application.core.domain.common.exception.BadRequestException;
 import br.com.managementfinanceapi.application.core.domain.common.exception.InvalidDateRangeException;
 import br.com.managementfinanceapi.application.core.domain.common.exception.NotFoundException;
 import br.com.managementfinanceapi.application.core.domain.transaction.TransactionDomain;
 import br.com.managementfinanceapi.application.core.domain.transaction.dvo.SearchAllFilters;
 import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
 import br.com.managementfinanceapi.application.port.out.transaction.SearchTransactionRespositoryPort;
+
+import java.util.List;
 
 public class SearchTransactionUseCase implements SearchTransactionPort {
 
