754f2f71a20456f9ec72ec1dfbe3798a93b4b403
Jonatas <delimajonatas875@gmail.com>
2025-07-01 20:11:43 -0300

    style: remove constructor unused

--- src/main/java/br/com/managementfinanceapi/application/core/domain/category/exception/CategoryAlreadyExistsException.java
+++ src/main/java/br/com/managementfinanceapi/application/core/domain/category/exception/CategoryAlreadyExistsException.java
@@ -3,10 +3,6 @@ package br.com.managementfinanceapi.application.core.domain.category.exception;
 import br.com.managementfinanceapi.application.core.domain.common.exception.ConflictException;
 
 public class CategoryAlreadyExistsException extends ConflictException {
-  public CategoryAlreadyExistsException(String message) {
-    super(message);
-
-  }
   public CategoryAlreadyExistsException() {
     super("Categoria já existe!");
   }
