4391a1872b4cdc3770bef74de3b5ea8828fa0283
Jonatas <delimajonatas875@gmail.com>
2025-07-05 12:00:17 -0300

    chore: use facade add transaction in controller

--- src/main/java/br/com/managementfinanceapi/adapter/in/controller/transaction/TransactionControllerV1.java
+++ src/main/java/br/com/managementfinanceapi/adapter/in/controller/transaction/TransactionControllerV1.java
@@ -1,5 +1,6 @@
 package br.com.managementfinanceapi.adapter.in.controller.transaction;
 
+import br.com.managementfinanceapi.application.port.in.transaction.AddUserTransactionPort;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,6 @@ import br.com.managementfinanceapi.adapter.in.dto.transaction.AddTransaction;
 import br.com.managementfinanceapi.application.core.domain.common.dvo.Page;
 import br.com.managementfinanceapi.application.core.domain.transaction.TransactionDomain;
 import br.com.managementfinanceapi.application.core.domain.transaction.dvo.SearchAllFilters;
-import br.com.managementfinanceapi.application.port.in.transaction.CreateTransactionPort;
 import br.com.managementfinanceapi.application.port.in.transaction.SearchTransactionPort;
 import jakarta.validation.Valid;
 
@@ -22,21 +22,21 @@ import jakarta.validation.Valid;
 @RequestMapping("/v1/transactions")
 public class TransactionControllerV1 {
 
-  private final CreateTransactionPort createTransactionPort;
+  private final AddUserTransactionPort addUserTransactionPort;
   private final SearchTransactionPort searchTransactionRepositoryPort;
 
 
   public TransactionControllerV1(
-    CreateTransactionPort createTransactionPort,
+    AddUserTransactionPort addUserTransactionPort,
     SearchTransactionPort searchTransactionRepositoryPort
   ) {
-    this.createTransactionPort = createTransactionPort;
+    this.addUserTransactionPort = addUserTransactionPort;
     this.searchTransactionRepositoryPort = searchTransactionRepositoryPort;
   }
 
   @PostMapping("/add")
   public ResponseEntity<ResponseV0<String>> add(@RequestBody AddTransaction body) {
-    this.createTransactionPort.execute(body.toDomain(), body.userId());
+    this.addUserTransactionPort.execute(body.toDomain(), body.userId());
     ResponseV0<String> addMovementResponse = ResponseV0.ok("Transação salva com sucesso");
     return ResponseEntity.ok(addMovementResponse);
   }
