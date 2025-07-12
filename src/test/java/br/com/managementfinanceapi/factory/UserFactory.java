cc1bcce279c5dc1b52d4dd4560a857732c004b84
Jonatas <delimajonatas875@gmail.com>
2025-07-12 10:20:05 -0300

    chore: change fixed user object to generate random email

--- src/test/java/br/com/managementfinanceapi/factory/UserFactory.java
+++ src/test/java/br/com/managementfinanceapi/factory/UserFactory.java
@@ -2,23 +2,12 @@ package br.com.managementfinanceapi.factory;
 
 import br.com.managementfinanceapi.adapter.out.entity.user.UserEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.UUID;
 
 public class UserFactory {
 
-  private static final int MAX_DATA_SIZE = 4;
-  private static final List<UserEntity> FAKE_DATA = new ArrayList<>(MAX_DATA_SIZE);
-  static {
-    FAKE_DATA.add(UserFactory.create("john@doe.com", "secretPassword"));
-    FAKE_DATA.add(UserFactory.create("carl@test.com", "secretPassword"));
-    FAKE_DATA.add(UserFactory.create("foo@bar.net", "secretPassword"));
-    FAKE_DATA.add(UserFactory.create("bar@foo.wa", "secretPassword"));
-  }
-
   public static UserEntity createFakeData(){
-    return FAKE_DATA.get(ThreadLocalRandom.current().nextInt(MAX_DATA_SIZE));
+    return UserFactory.create("john"+ UUID.randomUUID() + "@doe.test", "secretPassword");
   }
 
   public static UserEntity create(String email, String password){
