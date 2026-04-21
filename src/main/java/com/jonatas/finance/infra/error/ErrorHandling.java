3ec4068a66d02a1682a56633826aa2beda19428e
Jonatas <delimajonatas875@gmail.com>
2026-04-21 18:44:08 -0300

    fix: media type error result internal server error

--- src/main/java/com/jonatas/finance/infra/error/ErrorHandling.java
+++ src/main/java/com/jonatas/finance/infra/error/ErrorHandling.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,7 +32,7 @@ public class ErrorHandling {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Response<Void,Error<Map<String,String>>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
@@ -39,6 +40,11 @@ public class ErrorHandling {
         Error<Map<String,String>> contractViolation = new Error<>("contract_violation", errors);
         return ResponseEntity.badRequest()
             .body(Response.ofError(contractViolation, Response.Status.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(HttpMediaTypeException.class)
+    public ResponseEntity<Void> mediaTypeError(HttpMediaTypeException error) {
+        return ResponseEntity.status(error.getStatusCode()).build();
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
