a470b7263ed7aee5fd7f06a70ae57cc968bfb486
Jonatas <delimajonatas875@gmail.com>
2026-09-13 11:04:13 -0300

    test: add integration test case to edit wallet

--- src/test/java/com/jonatas/finance/integrationTest/WalletControllerIT.java
+++ src/test/java/com/jonatas/finance/integrationTest/WalletControllerIT.java
@@ -1,17 +1,25 @@
 package com.jonatas.finance.integrationTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.jonatas.finance.auth.UserRepository;
+import com.jonatas.finance.wallet.Wallet;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 class WalletControllerIT extends BaseIntegratioTest {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private UserRepository userRepository;
@@ -36,4 +44,38 @@ class WalletControllerIT extends BaseIntegratioTest {
                .andExpect(jsonPath("$.data[0].name").value(name))
                .andExpect(jsonPath("$.data[0].mainWallet").value(true));
     }
+
+    @Test
+    void shouldEditWallet() throws Exception {
+        var email = "wallet@controller.test";
+        TestUtils.createUser(userRepository, passwordEncoder, email);
+        var token = TestUtils.loginAndGetAccessToken(mockMvc, email);
+        var name = "Banco Teste";
+        Long walletId = TestUtils.createWallet(mockMvc, token, name, true);
+
+        Wallet wallet = entityManager.find(Wallet.class, walletId);
+        assertThat(wallet).isNotNull();
+        assertThat(wallet.getId()).isEqualTo(walletId);
+        assertThat(wallet.getDescriptionValue()).isEqualTo(name);
+        assertThat(wallet.isMain()).isTrue();
+
+        String editedName = "WalletEdited";
+        mockMvc.perform(put("/v1/wallets/{id}", walletId)
+                                               .header("Authorization", "Bearer " + token)
+                                               .contentType(MediaType.APPLICATION_JSON)
+                                               .content("""
+                                                        {
+                                                            "name": "%s",
+                                                            "mainWallet": false
+                                                        }
+                                                        """.formatted(editedName)))
+               .andExpect(status().isNoContent());
+
+        Wallet walletEdited = entityManager.find(Wallet.class, walletId);
+        assertThat(walletEdited).isNotNull();
+        assertThat(walletEdited.getId()).isEqualTo(walletId);
+        assertThat(walletEdited.getDescriptionValue()).isEqualTo(editedName);
+        assertThat(walletEdited.isMain()).isFalse();
+    }
+
 }
